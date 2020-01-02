b5efa11211a063ad461d9b34f930378813781e5e
Jordy Aaldering <aalderingjordy@gmail.com>
2020-01-02 16:01:01 +0100

    Chunk Loading

--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -1,18 +1,47 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class World : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float chunkSpeed = 1f;
+    [SerializeField] private float minX = -10f;
+    
+    [SerializeField] private Chunk[] chunks = new Chunk[4];
+    [SerializeField] private Chunk[] chunkPrefabs = new Chunk[0];
+
+    private void Awake()
     {
-        
+        for (int i = 1; i < chunks.Length; i++)
+        {
+            if (chunks[i] != null)
+                continue;
+
+            Vector2 pos = (Vector2) chunks[i - 1].endPoint.position - (Vector2) chunks[i - 1].startPoint.localPosition;
+            Chunk chunk = Instantiate(chunkPrefabs[Random.Range(0, chunkPrefabs.Length - 1)], pos, Quaternion.identity, transform);
+            chunks[i] = chunk;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        for (int i = 0; i < chunks.Length; i++)
+        {
+            Chunk chunk = chunks[i];
+            chunk.transform.position += chunkSpeed * Time.deltaTime * Vector3.left;
+            
+            if (chunks[i].endPoint.position.x < minX)
+                SetChunk(i);
+        }
+    }
+
+    private void SetChunk(int index)
+    {
+        int prev = index - 1;
+        if (prev < 0) prev += chunks.Length;
         
+        Destroy(chunks[index].gameObject);
+
+        Vector2 pos = (Vector2) chunks[prev].endPoint.position - (Vector2) chunks[prev].startPoint.localPosition;
+        Chunk chunk = Instantiate(chunkPrefabs[Random.Range(0, chunkPrefabs.Length - 1)], pos, Quaternion.identity, transform);
+        chunks[index] = chunk;
     }
 }
